eb86bca4e6e5fc3be2d64428fea578ac2fc87f39
gustavonunes0 <gustavonoroes@outlook.com>
2023-09-10 21:37:19 -0300

    changes

--- src/components/IntroProjetos/index.tsx
+++ src/components/IntroProjetos/index.tsx
@@ -1,12 +1,17 @@
 import * as S from "./styles";
 import Link from "next/link";
-const IntroProjetos = ({ nome }) => {
+
+interface IntroProjetosProps {
+  nome: string;
+}
+
+const IntroProjetos: React.FC<IntroProjetosProps> = ({ nome }) => {
   const nomeEmMaiusculas = nome.toUpperCase();
 
   return (
-    <div style={{padding: '0 7rem 5rem 7rem', width: '100%', display: "flex", justifyContent: "center"}}>
-      <div style={{width: '80%'}}>
-        <div style={{ display: "flex", alignItems: "center", gap: ".8rem", width:'100%' }}>
+    <div style={{ padding: '0 7rem 5rem 7rem', width: '100%', display: "flex", justifyContent: "center" }}>
+      <div style={{ width: '80%' }}>
+        <div style={{ display: "flex", alignItems: "center", gap: ".8rem", width: '100%' }}>
           <S.SpanNome>{nomeEmMaiusculas}</S.SpanNome>
           <S.divLinhaAzul></S.divLinhaAzul>
         </div>
